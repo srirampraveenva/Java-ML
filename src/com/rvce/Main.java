0add0c3148c8d511065670b1f133a8265fcd6f75
Sriram Praveen <srirampraveenva@gmail.com>
2021-06-25 09:55:43 +0530

    Added Project

---
+++ src/com/rvce/Main.java
@@ -0,0 +1,10 @@
+package com.rvce;
+import weka.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hey Weka");
+    }
+}
